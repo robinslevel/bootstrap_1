d6b8596822c6e6189c3061701813df13c0d915b9
robinslevel <robin.korkor@hva.nl>
2021-08-26 06:47:20 +0200

    first commit

---
+++ java/script.java
@@ -0,0 +1,4 @@
+console.log('hello world');
+var img_circle = document.getElementById('rounded-circle mb-3');
+var password = document.getElementById('bi bi-lock bg-secondary');
+console.log(password);
